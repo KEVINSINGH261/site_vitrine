715648f403310d45215c0fc449a91c07d5398df4
KEVINSINGH261 <kevin.singh261@gmail.com>
2025-01-20 12:28:33 +0100

    fdfd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AccueilComponent } from './components/accueil/accueil.component';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AccueilComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './components/accueil/accueil.component';
+import { ProgrammeComponent } from './components/programme/programme.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/accueil', pathMatch: 'full' },
+    { path: 'accueil', component: AccueilComponent },
+    { path: 'programme', component: ProgrammeComponent },
+];
