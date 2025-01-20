2bf27736bca9ddf4a3601948d69908c5b39dfaf6
KEVINSINGH261 <kevin.singh261@gmail.com>
2025-01-20 17:34:00 +0100

    presque fini

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { AccueilComponent } from './components/accueil/accueil.component';
 import { ProgrammeComponent } from './components/programme/programme.component';
+import { AProposComponent } from './components/a-propos/a-propos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/accueil', pathMatch: 'full' },
     { path: 'accueil', component: AccueilComponent },
     { path: 'programme', component: ProgrammeComponent },
+    { path: 'apropos', component: AProposComponent },
+    { path: 'footer', component: FooterComponent },
+
 ];
